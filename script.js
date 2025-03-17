d98f8cd4e1d2c227aee81486337f289bb60b0de6
ProjectsDisasters <schoolminecra@gmail.com>
2025-03-17 11:37:43 +0100

    yes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("showMessageButton").addEventListener("click", function() {
+    var explanation = document.getElementById("explanation");
+    explanation.classList.toggle("hidden"); // Toggle de zichtbaarheid van de uitleg
+});
